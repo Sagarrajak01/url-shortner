3a8543413152a80801c8f18784cfc838456f2b2e
Sagar Rajak <sagarrajak01@gmail.com>
2026-03-11 23:38:58 +0530

    Added error handling

---
+++ BACKEND/src/utils/errorHandler.js
@@ -0,0 +1,28 @@
+export class AppError extends Error {
+    constructor(message, statusCode) {
+        super(message)
+        this.statusCode = statusCode
+        this.success = false
+        this.isOperational = true
+        Error.captureStackTrace(this, this.constructor)
+    }
+}
+
+export const errorHandler = (err, req, res, next) => {
+    let statusCode = err.statusCode || 500
+    let message = err.message || "Internal Server Error"
+
+    if (err instanceof AppError) {
+        return res.status(statusCode).json({
+            success: false,
+            message
+        })
+    }
+
+    console.error(err)
+
+    res.status(statusCode).json({
+        success: false,
+        message
+    })
+}
