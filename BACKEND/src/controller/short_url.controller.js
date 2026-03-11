dc64f4807b4991e71a36cc499ac270436754c87f
Sagar Rajak <sagarrajak01@gmail.com>
2026-03-11 23:45:28 +0530

    update controller

--- BACKEND/src/controller/short_url.controller.js
+++ BACKEND/src/controller/short_url.controller.js
@@ -1,7 +1,14 @@
-import { createShortUrlService } from "../service/short_url.service.js"
+import { createShortUrlWithoutUserService } from "../service/short_url.service.js"
+import { getShortUrl } from "../dao/short_url.js"
 
 export const  createShortUrl = async (req, res) => {
     const { url } = req.body
-    const shortUrl = await createShortUrlService(url)
+    const shortUrl = await createShortUrlWithoutUserService(url)
     res.json(process.env.APP_URL + shortUrl)
 }
+
+export const redirectFromShortUrl = async (req, res) => {
+    const { id } = req.params
+    const url = await getShortUrl(id)
+    res.redirect(url.full_url)
+}   
