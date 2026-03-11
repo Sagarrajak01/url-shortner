b35088d8059b05759c1e0dc5280baad1c1289297
Sagar Rajak <sagarrajak01@gmail.com>
2026-03-11 23:43:55 +0530

    Integrate error handling

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -3,8 +3,9 @@ dotenv.config()
 
 import express from "express"
 import connectDB from "./src/config/mongo.config.js"
-import urlSchema from "./src/models/short_url.model.js"
 import short_url from "./src/route/short_url.route.js"
+import { redirectFromShortUrl } from "./src/controller/short_url.controller.js"
+import { errorHandler } from "./src/utils/errorHandler.js"
 
 const app = express()
 const PORT = 3000
@@ -13,17 +14,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use("/api/create", short_url)
-
-app.get("/:id", async (req, res) => {
-    const { id } = req.params
-    const url = await urlSchema.findOne({ short_url: id })
-    if (url) {
-        res.redirect(url.full_url)
-    }
-    else {
-        res.status(404).send("Not Found")
-    }
-})
+app.get("/:id", redirectFromShortUrl)
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     connectDB()
