5d84bb4ac09e13382b2650af0ba3e553b7c76e93
Sagar Rajak <sagarrajak01@gmail.com>
2026-03-13 23:14:20 +0530

    Refactor controllers to use wrapAsync for error handling

--- BACKEND/src/controller/short_url.controller.js
+++ BACKEND/src/controller/short_url.controller.js
@@ -1,14 +1,18 @@
 import { createShortUrlWithoutUserService } from "../service/short_url.service.js"
 import { getShortUrl } from "../dao/short_url.js"
+import { wrapAsync } from "../utils/tryCatchWrapper.js"
 
-export const  createShortUrl = async (req, res) => {
+export const createShortUrl = wrapAsync(async (req, res) => {
     const { url } = req.body
     const shortUrl = await createShortUrlWithoutUserService(url)
     res.json(process.env.APP_URL + shortUrl)
-}
+})
 
-export const redirectFromShortUrl = async (req, res) => {
+export const redirectFromShortUrl = wrapAsync(async (req, res) => {
     const { id } = req.params
     const url = await getShortUrl(id)
+    if (!url) {
+            throw new Error("URL not found")
+    }
     res.redirect(url.full_url)
-}   
+})
